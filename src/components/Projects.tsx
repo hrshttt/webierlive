76b6569164d57f2383cffdf0374acd68dd2e50ce
Ashwin <chhipaashwin13@gmail.com>
2026-02-04 23:40:42 +0530

    again trying

--- src/components/Projects.tsx
+++ src/components/Projects.tsx
@@ -2,7 +2,7 @@ import { useRef } from "react";
 import gsap from "gsap";
 import { useGSAP } from "@gsap/react";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ArrowUpRight, Plus, Monitor, Scan, Info } from "lucide-react";
+import { ArrowUpRight, Plus, Info } from "lucide-react";
 
 gsap.registerPlugin(ScrollTrigger);
 
